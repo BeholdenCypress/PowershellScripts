a4a13ee78af6ec62766f9abcb059672ac5c7a23b
BeholdenCypress <kaceho18@gmail.com>
2021-07-20 14:39:03 -0700

    Add files via upload

---
+++ Password Expirtation Bot.ps1
@@ -0,0 +1,10 @@
+SamAccountName       EmailAddress                     PasswordExpiry
+--------------       ------------                     --------------
+ExampleText          emial.address@domain.com         10/4/2021 10:51:23 AM
+ExampleText          emial.address@domain.com         7/28/2021 10:46:33 AM
+ExampleText          emial.address@domain.com         9/8/2021 2:58:42 PM
+ExampleText          emial.address@domain.com         7/27/2021 8:25:41 AM
+ExampleText          emial.address@domain.com         9/26/2021 3:32:59 PM
+ExampleText          emial.address@domain.com         10/17/2021 7:03:20 AM
+ExampleText          emial.address@domain.com         9/2/2021 2:51:47 PM
+ExampleText          emial.address@domain.com         9/29/2021 3:51:12 PM
